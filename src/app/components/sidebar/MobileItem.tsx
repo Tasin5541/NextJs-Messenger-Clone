04d3f8be582d049c2f4c27a42af3fb674c3043f4
Tasin5541 <tasinmiftaulmannan@gmail.com>
2023-05-23 13:22:50 +0600

    Add [Sidebar]: Mobile footer

---
+++ src/app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,46 @@
+import clsx from "clsx";
+
+import Link from "next/link";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({ href, icon: Icon, active, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <Link
+      onClick={handleClick}
+      href={href}
+      className={clsx(
+        `
+        group 
+        flex 
+        gap-x-3 
+        text-sm 
+        leading-6 
+        font-semibold 
+        w-full 
+        justify-center 
+        p-4 
+        text-gray-500 
+        hover:text-black 
+        hover:bg-gray-100
+      `,
+        active && "bg-gray-100 text-black"
+      )}
+    >
+      <Icon className="h-6 w-6" />
+    </Link>
+  );
+};
+
+export default MobileItem;
--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
