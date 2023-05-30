415bd4af84d6e00f0e11d8e62d8124f90158b610
Tasin5541 <tasinmiftaulmannan@gmail.com>
2023-05-30 19:04:57 +0600

    Fix [Dark Theme]: toast notification color

--- src/app/context/ToasterContext.tsx
+++ src/app/context/ToasterContext.tsx
@@ -3,7 +3,13 @@
 import { Toaster } from "react-hot-toast";
 
 const ToasterContext = () => {
-  return <Toaster />;
+  return (
+    <Toaster
+      toastOptions={{
+        className: "dark:bg-lightgray dark:text-gray-200",
+      }}
+    />
+  );
 };
 
 export default ToasterContext;
